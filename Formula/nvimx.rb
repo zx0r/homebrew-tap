325b9df2aa809974eb4ebfa1ae339e8857d6fa65
zx0r <117382621+zx0r@users.noreply.github.com>
2026-04-28 21:39:55 +0300

    fix: restore flat if/elsif structure to fix Homebrew parsing (no classes found)

--- Formula/nvimx.rb
+++ Formula/nvimx.rb
@@ -2,26 +2,21 @@ class Nvimx < Formula
   desc "Fast and flexible Neovim profile manager"
   homepage "https://github.com/zx0r/nvimx"
   version "0.1.0"
-  if OS.mac?
-    if Hardware::CPU.arm?
-      url "https://github.com/zx0r/nvimx/releases/download/v0.1.0/nvimx-aarch64-apple-darwin.tar.gz"
-      sha256 "caaaec1e17325d23ddf0815e56ababe1264e15c953223dbf134de8f0558283cf"
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/zx0r/nvimx/releases/download/v0.1.0/nvimx-x86_64-apple-darwin.tar.gz"
-      sha256 "de3fddd93ebb1c00f27229802089ca90339564dea25bea0fcee8d04c726f60a5"
-    end
+
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/zx0r/nvimx/releases/download/v0.1.0/nvimx-aarch64-apple-darwin.tar.gz"
+    sha256 "caaaec1e17325d23ddf0815e56ababe1264e15c953223dbf134de8f0558283cf"
+  elsif OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/zx0r/nvimx/releases/download/v0.1.0/nvimx-x86_64-apple-darwin.tar.gz"
+    sha256 "de3fddd93ebb1c00f27229802089ca90339564dea25bea0fcee8d04c726f60a5"
+  elsif OS.linux? && Hardware::CPU.arm?
+    url "https://github.com/zx0r/nvimx/releases/download/v0.1.0/nvimx-aarch64-unknown-linux-gnu.tar.gz"
+    sha256 "d01205ff9bab08f33e597c54e392bd67ed0d4a7286ce2e1f359c41c34bcb4e18"
+  elsif OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/zx0r/nvimx/releases/download/v0.1.0/nvimx-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "d0074b0a68c3b9a44536352c6a18006522970cfbf8135caeaacedbc3f2984242"
   end
-  if OS.linux?
-    if Hardware::CPU.arm?
-      url "https://github.com/zx0r/nvimx/releases/download/v0.1.0/nvimx-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "d01205ff9bab08f33e597c54e392bd67ed0d4a7286ce2e1f359c41c34bcb4e18"
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/zx0r/nvimx/releases/download/v0.1.0/nvimx-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "d0074b0a68c3b9a44536352c6a18006522970cfbf8135caeaacedbc3f2984242"
-    end
-  end
+
   license "MIT"
 
   BINARY_ALIASES = {
@@ -51,10 +46,7 @@ class Nvimx < Formula
   end
 
   def install
-    bin.install "nvimx" if OS.mac? && Hardware::CPU.arm?
-    bin.install "nvimx" if OS.mac? && Hardware::CPU.intel?
-    bin.install "nvimx" if OS.linux? && Hardware::CPU.arm?
-    bin.install "nvimx" if OS.linux? && Hardware::CPU.intel?
+    bin.install "nvimx"
 
     install_binary_aliases!
 
